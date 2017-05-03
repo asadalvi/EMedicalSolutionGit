5f1e5efeffe7c0f4c3c8a9849b9df1ba45531c6b
naveedshahji <naveed.shah194@gmail.com>
2017-05-03 11:48:52 +0500

    add excel report

--- EMedicalSolution/EMedicalSolution/Controllers/PatientsController.cs
+++ EMedicalSolution/EMedicalSolution/Controllers/PatientsController.cs
@@ -8,6 +8,8 @@ using EMedicalSolution.ViewModels;
 using Newtonsoft.Json;
 using System.IO;
 using HiQPdf;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace EMedicalSolution.Controllers
 {
@@ -988,5 +990,42 @@ namespace EMedicalSolution.Controllers
         //}
         return View(intakeView);
     }
-}
+        //excel
+        public ActionResult ExportToExcel(int id)
+        {
+            var products = new System.Data.DataTable("teste");
+            products.Columns.Add("col1", typeof(int));
+            products.Columns.Add("col2", typeof(string));
+
+            products.Rows.Add(1, "product 1");
+            products.Rows.Add(2, "product 2");
+            products.Rows.Add(3, "product 3");
+            products.Rows.Add(4, "product 4");
+            products.Rows.Add(5, "product 5");
+            products.Rows.Add(6, "product 6");
+            products.Rows.Add(7, "product 7");
+
+
+            var grid = new GridView();
+            grid.DataSource = products;
+            grid.DataBind();
+
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=MyExcelFile.xls");
+            Response.ContentType = "application/ms-excel";
+
+            Response.Charset = "";
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            grid.RenderControl(htw);
+
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
+
+            return View();
+        }
+    }
 }
